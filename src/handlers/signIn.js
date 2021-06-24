cb69d832fc2b63e63d6b1d1c46cd1c5c6224df95
Mohammad <malnajjar16@gmail.com>
2021-06-24 23:22:59 +0200

    signOut btn added+ signedIn status added to local

--- src/handlers/signIn.js
+++ src/handlers/signIn.js
@@ -6,7 +6,7 @@ import getDomElement from "../utils/getDomElement.js";
 
 export const signIn = (e, usernameInput, passwordInput) => {
   const signInContainer = getDomElement(SIGN_IN_CONTAINER);
-
+  let wrongPass = 0;
   let savedUsers = [];
   for (const key in localStorage) {
     if (key.includes("user")) {
@@ -19,24 +19,27 @@ export const signIn = (e, usernameInput, passwordInput) => {
       const { userName, passWord } = user;
       if (userName === usernameInput && passWord === passwordInput) {
         // username + password correct
-        console.log("signed in");
+        localStorage.setItem(
+          "status",
+          JSON.stringify({
+            signedIn: true,
+          })
+        );
         initializeApplication();
         return user;
       } else if (userName === usernameInput && passWord !== passwordInput) {
         // password incorrect
-
+        wrongPass++;
         const passWrong = createDomElement("h3", { className: "passWrong" });
         passWrong.textContent = "Password is incorrect!";
         signInContainer.appendChild(passWrong);
         setTimeout(() => {
           passWrong.remove();
         }, 2000);
-
-        console.log("incorrect pass");
       }
     });
 
-    if (foundUser.length == 0) {
+    if (foundUser.length == 0 && wrongPass === 0) {
       // user not found
       const noUser = createDomElement("h3", { className: "no-user" });
       noUser.textContent = "This user name is not registered yet!";
@@ -44,11 +47,16 @@ export const signIn = (e, usernameInput, passwordInput) => {
       setTimeout(() => {
         noUser.remove();
       }, 2000);
-
-      console.log("USERNAME NOT FOUND");
     }
   } else {
-    console.log("please fill in username and password");
+    // no input
+    const noInput = createDomElement("h3", { className: "no-input" });
+    noInput.textContent = "Please fill in username and password!";
+    signInContainer.appendChild(noInput);
+    setTimeout(() => {
+      noInput.remove();
+    }, 2000);
+
     //   please fill an username and password
   }
 };
