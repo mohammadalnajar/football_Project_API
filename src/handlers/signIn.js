4e3aa2657849b5fd4339e0899cf1b3ada6a79514
Mohammad <malnajjar16@gmail.com>
2021-06-24 15:54:40 +0200

    check if username and password are correct

--- src/handlers/signIn.js
+++ src/handlers/signIn.js
@@ -1,7 +1,6 @@
 import { initializeApplication } from "../init/start.js";
 
-export const signIn = (e, username, password) => {
-  //   console.log(userName, password);
+export const signIn = (e, usernameInput, passwordInput) => {
   let savedUsers = [];
   for (const key in localStorage) {
     if (key.includes("user")) {
@@ -9,22 +8,27 @@ export const signIn = (e, username, password) => {
       savedUsers.push(user);
     }
   }
-  if (username && password) {
-    savedUsers.forEach((user) => {
+  if (usernameInput && passwordInput) {
+    const foundUser = savedUsers.filter((user) => {
       const { userName, passWord } = user;
-      if (userName === username) {
-        if (passWord === password) {
-          console.log("signed in");
-          initializeApplication();
-        } else {
-          console.log("incorrect password");
-        }
+      if (userName === usernameInput && passWord === passwordInput) {
+        // username + password correct
+        console.log("signed in");
+        initializeApplication();
+        return user;
+      } else if (userName === usernameInput && passWord !== passwordInput) {
+        // password incorrect
+        console.log("incorrect pass");
       }
     });
+    if (foundUser.length == 0) {
+      // user not found
+      console.log("USERNAME NOT FOUND");
+    }
   } else {
     console.log("please fill in username and password");
     //   please fill an username and password
   }
-  console.log(savedUsers);
+  // console.log(savedUsers);
 };
 export default signIn;
