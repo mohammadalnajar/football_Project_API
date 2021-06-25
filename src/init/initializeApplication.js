881ae903d9d7389bd925d24a8747d14f081f2614
Mohammad <malnajjar16@gmail.com>
2021-06-25 21:29:52 +0200

    search controls added

--- src/init/initializeApplication.js
+++ src/init/initializeApplication.js
@@ -1,8 +1,10 @@
 import { ROOT_ID } from "../constants.js";
 import signOut from "../handlers/signOut.js";
 import clearDomElement from "../utils/clearDomElement.js";
+import createDomElement from "../utils/createDomElement.js";
 import getDomElement from "../utils/getDomElement.js";
 import { createButton } from "../views/createButton.js";
+import createSearchControls from "../views/createSearchControls.js";
 
 export const initializeApplication = () => {
   clearDomElement(ROOT_ID);
@@ -11,6 +13,15 @@ export const initializeApplication = () => {
   signOutBtn.addEventListener("click", () => {
     signOut();
   });
+  const bigContainer = createDomElement("div", { className: "container" });
+  const flexContainer = createDomElement("div", {
+    className: "d-flex flex-column",
+  });
+  const searchControls = createSearchControls();
+
+  flexContainer.appendChild(searchControls);
+  bigContainer.appendChild(flexContainer);
+  root.appendChild(bigContainer);
   root.appendChild(signOutBtn);
 };
 export default initializeApplication;
