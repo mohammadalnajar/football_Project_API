069d9ea4dd99a096978f11a635f7df208190844c
Mohammad <malnajjar16@gmail.com>
2021-06-26 17:53:15 +0200

    last basis added to the league content

--- src/views/createLeagueMatches.js
+++ src/views/createLeagueMatches.js
@@ -1,2 +1,16 @@
-export const createLeagueMatches = (league) => {};
+import setAttributes from "../handlers/setAttributes.js";
+import createDomElement from "../utils/createDomElement.js";
+
+export const createLeagueMatches = (league) => {
+  const matchesDiv = createDomElement("div", {
+    className: "tab-pane fade",
+    id: "nav-contact",
+  });
+  setAttributes(matchesDiv, {
+    role: "tabpanel",
+    "aria-labelledby": "nav-contact-tab",
+  });
+  matchesDiv.innerHTML = "Matches";
+  return matchesDiv;
+};
 export default createLeagueMatches;
