eefd2b076f71cb77aa46cd03987ca6078e60dbec
Mohammad <malnajjar16@gmail.com>
2021-06-29 11:59:02 +0200

    add the brazilian league to show the rror handeling

--- src/data.js
+++ src/data.js
@@ -8,7 +8,9 @@ export const Data = {
     },
   },
   selectedTeam: "",
-  leaguesIds: [4328, 4331, 4332, 4334, 4335, 4337, 4338, 4339, 4753, 4668],
+  leaguesIds: [
+    4328, 4331, 4332, 4334, 4335, 4337, 4338, 4339, 4753, 4668, 4351,
+  ],
   leagues: "",
   teamsIcons: {},
 };
